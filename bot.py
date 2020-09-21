fe546b139a2c2731d0706c2740c203777cd4d9ad
sanayut12345 <sanayut.t@live.ku.th>
2020-09-21 07:05:14 -0700

    update

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
 import gspread
+import datetime
 
+#timing
+get_datetime = datetime.datetime.now()
+date = get_datetime.date()
+time = get_datetime.timetuple()
+#google sheet
 gc = gspread.service_account(filename='service.json')
 sh = gc.open_by_key('1WKj5jeVEsa0TA-sv7j_sGlcbZRNUgJJfgpk92SRPpKA')
-worksheet = sh.worksheet("student")
-
+worksheet = sh.worksheet("Suser")
+body = [5888,str(date),str(time)]
 res = worksheet.get_all_records()
+index = len(res)+2
 
-print(res)
+worksheet.insert_row(body,index=index)
---
+++ time.py
@@ -0,0 +1,13 @@
+import datetime  
+    
+# using now() to get current time  
+current_time = datetime.datetime.now()  
+timenow = datetime.datetime.now().time()
+# Printing value of now.  
+print ("Time now at greenwich meridian is : "
+                                    , end = "")  
+print (current_time)  
+
+print ("Time now at greenwich meridian is : "
+                                    , end = "")  
+print (timenow)  
